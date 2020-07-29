f36bc2c21dca790f7e2640bfdbb981ee8477f64f
schaetay <62626653+schaetay@users.noreply.github.com>
2020-07-29 14:31:28 -0600

    Update Q_5.sql

--- LAB 8/Q_5.sql
+++ LAB 8/Q_5.sql
@@ -1,20 +1,32 @@
 GO
-ALTER PROCEDURE UpdateProductPrice
+CREATE OR ALTER PROCEDURE UpdateProductPrice
 	( 
 	@ProductID SMALLINT,
 	@Price DECIMAL(7,2)
 	)
 AS
 BEGIN
-  DECLARE
-	
-	START
 
-		UPDATE SalesPrice = @SalesPrice
-		SET PriceChangeDate = CURRENT_DATE()
-		INSERT ProductPriceHistory;
+	SET XACT_ABORT ON;
+	BEGIN TRANSACTION
+	--UPDATE ProductPrice
 
+		UPDATE SalesPrice
+		SET PriceChangeDate = GETDATE()
+		WHERE ProductID = @ProductID;
 
-	COMMIT;
+	COMMIT TRANSACTION;
+
+	BEGIN
+
+		INSERT INTO ProductPriceHistory (ProductID, PriceChangeDate, SalesPrice)
+		VALUES (@ProductID, NULL, @Price);
+		 
+	END
+	SET XACT_ABORT OFF;
 
 END
+
+EXEC UpdateProductPrice
+	@ProductID = '1',
+	@Price = '55.00'
